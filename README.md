449a0628360a8571dd2519276edaea21483e6c44
João Pedro Ougano <87842758+JPougano@users.noreply.github.com>
2022-03-04 01:50:38 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+Esse é mais um desafio do Frontend Mentor!
+
+Pessoal, to muito feliz com o resultado desse projeto! Embora muito simples, foi uma das primeiras vezes que consegui trabalhar com API e entender um pouco mais
+sobre os conceitos de GET!
+
+Por incrivel que pareça eu fiz todo esse projeto em menos de 1hora e meia e não tive muita dificuldade.
+
+O HTML e o CSS foram 'triviais' e, a cada dia que passa, se tornam mais a minha zona de conforto. 
+O JS também não foi muito complexo, só precisando dar uma lida rápida na documentação do XMLRequest pra poder escrever a estrutura.
+
+No final, acho que esse projeto serviu pra abrir portas pro mundo das APIs e me dar coragem pra pegar desafios mais complexos!
+
+
+Vamos que vamos que vocês ainda vão me ver muito por aqui! :)
---
+++ index.js
@@ -0,0 +1,33 @@
+let idField = document.querySelector('.advice__number');
+let adviceField = document.querySelector('.advice');
+
+
+function getAdvices(url){
+    let request = new XMLHttpRequest();
+    request.open("GET",url,false);
+    request.send();
+    return request.responseText;
+}
+
+function usingGet(){
+    data = getAdvices("https://api.adviceslip.com/advice");
+
+    adviceData = JSON.parse(data);
+
+    var id = adviceData.slip.id;
+    var advice = adviceData.slip.advice;
+
+    idField.textContent = ("ADVICE #" + id)
+    adviceField.textContent = ('"' + advice + '"')
+}
+
+function paginaCarregou(cb) {
+    (function carregamentoCompleto() {
+      var state = document.readyState;
+      if (state == 'loaded' || state == 'complete') 
+      usingGet();
+      else setTimeout(carregamentoCompleto, 200);
+    })();
+  };
+
+  paginaCarregou()
